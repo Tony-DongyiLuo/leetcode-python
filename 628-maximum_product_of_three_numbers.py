d4434870f90c0108c95ae8b38e349bb5b9322d24
Tony Luo <luodongyi_career@163.com>
2017-06-26 16:38:39 +0800

    628.py

---
+++ 628-maximum_product_of_three_numbers.py
@@ -0,0 +1,26 @@
+#python3
+'''
+Given an integer array, find three numbers whose product is maximum and output the maximum product.
+
+Example 1:
+Input: [1,2,3]
+Output: 6
+Example 2:
+Input: [1,2,3,4]
+Output: 24
+Note:
+The length of the given array will be in range [3,104] and all elements are in the range [-1000, 1000].
+Multiplication of any three numbers in the input won't exceed the range of 32-bit signed integer.
+'''
+def maximumProduct(nums):
+	nums.sort()
+	posResult = nums[-1] * nums[-2] * nums[-3]
+	negResult = nums[0] * nums[1] * nums[-1]
+	
+	return max(posResult, negResult)
+	
+	
+
+
+if __name__ == '__main__':
+	print(maximumProduct([-1,-2,-3,-4,60]))
